4063b3034309a812c41d64392b85c4e44ade0255
Danila Belous <belous.dp@gmail.com>
2023-11-24 19:43:41 +0500

    tapes documentation added

--- basic_tape.h
+++ basic_tape.h
@@ -5,14 +5,55 @@
 #include <memory>
 #include <optional>
 
-// const basic_tape == read-only
+/**
+ * Interface that abstracts a tape.
+ * const basic_tape means read-only tape.
+ * All methods can block.
+ * Empty elements are allowed.
+ */
 struct basic_tape {
+    /**
+     * Reads current element from the tape.
+     * @return current element of the tape
+     */
     virtual int read() const = 0; // NOLINT(*-use-nodiscard)
+
+    /**
+     * If current element is empty, the method returns null optional.
+     * Otherwise the method returns the same data as basic_tape::read(),
+     * but (usually) works slower.
+     * @return optionally returns current element of the tape
+     */
     virtual std::optional<int> read_safe() const = 0; // NOLINT(*-use-nodiscard)
+
+    /**
+     * Writes `data` to the current position on the tape.
+     * @param data data to write
+     */
     virtual void write(int data) = 0;
+
+    /**
+     * Moves left reading/writing head of the tape.
+     * If the head is already on the left-most position, does nothing.
+     * @return true the head of the tape was moved left, false otherwise
+     */
     virtual bool move_left() const = 0; // NOLINT(*-use-nodiscard)
+
+    /**
+     * Moves right reading/writing head of the tape.
+     * If the head is already on the right-most position, does nothing.
+     * @return true the head of the tape was moved right, false otherwise
+     */
     virtual bool move_right() const = 0; // NOLINT(*-use-nodiscard)
+
+    /**
+     * Destroys the tape. Frees resources.
+     */
     virtual ~basic_tape() = default;
+
+    /**
+     * Sets the head of the tape in left-most position.
+     */
     virtual void rewind() const {
         while (move_left());
     }
--- file_tape.h
+++ file_tape.h
@@ -6,6 +6,18 @@
 #include <string>
 #include <fstream>
 
+/**
+ * This class emulates a tape by interacting with a text file.
+ * The tape is stored integer (int) data.
+ * The tape data is stored (and parsed) as follows:
+ * "          1         123    10000000             -1234567890 "
+ * That is, to store each number,
+ * <11 minus the length of string representation of the number> whitespaces is written,
+ * then the number is written.
+ * The elements are separated by whitespace.
+ * Empty elements are allowed (stored as 11 whitespaces).
+ * For emulate delays in IO operations, timings from timings_config class are used.
+ */
 class file_tape : public basic_tape {
 public:
     struct timings_config {
@@ -16,11 +28,41 @@ public:
         uint32_t rewind = 0;
 
         timings_config() = default;
+        /**
+         * Attempts to parse `filename` and update default timings values from a content of the file.
+         * Allowed format: 'r' or "read" for read timing, 'w' or "write" for write timing,
+         * "ml" or "move_left" for move_left timing, "mr" or "move_right" for move_right timing,
+         * "rewind" for "rewind" timing.
+         * Field name must be followed by '=' and field value.
+         * Example: "mr=1337 r=123  move_left=42 rewind=101" (without quotes).
+         * Result: { .read=123, .write=0, .move_left=42, .move_right=1337, .rewind=101 }.
+         * @param filename path to the config file
+         */
         explicit timings_config(std::string const& filename);
         [[nodiscard]] std::string to_string() const;
     };
 
+    /**
+     * Creates file_tape that stores data in the given file. The file must has one of the following states:
+     * 1. it can contains data in valid format (described above);
+     * 2. it can be empty, in that case the file is filled (pre-allocated) with required number of whitespaces;
+     * 3. it can yet not exists, in that case an attempt to create the file is being made, then see case 2.
+     * Default timings are used.
+     * @param filename path to the file where to store data
+     * @param size number of elements of the tape
+     */
     file_tape(std::string const& filename, size_t size);
+
+    /**
+     * Creates file_tape that stores data in the given file. The file must has one of the following states:
+     * 1. it can contains data in valid format (described above);
+     * 2. it can be empty, in that case the file is filled (pre-allocated) with required number of whitespaces;
+     * 3. it can yet not exists, in that case an attempt to create the file is being made, then see case 2.
+     * Attempts to parse timings from `config_filename` path. See timings_config::timings_config(std::string const&).
+     * @param filename path to the file where to store data
+     * @param size number of elements of the tape
+     * @param config_filename path to the timings configuration file
+     */
     file_tape(std::string const& filename, size_t size, std::string const& config_filename);
 
     int read() const override;
